ab9b6be55a4fdec5ca6448f11c0c98b79c35fafa
Thibault Touzin <thibault.touzin@gmail.com>
2022-07-18 12:02:01 +0200

    nearly finished projet

--- src/Controller/AdminAuthorsController.php
+++ src/Controller/AdminAuthorsController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\Entity\Author;
 use App\Form\AuthorType;
 use App\Repository\AuthorRepository;
+use App\Repository\BookRepository;
+use App\Repository\CategoryRepository;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -81,6 +83,37 @@ class AdminAuthorsController extends AbstractController
         }
         return $this->render("/admin/new-author.html.twig",['form'=>$form->createView()]);
 //        dd("coucou");
+    }
+
+    /**
+     * @Route("/admin/authors/search", name="admin-authors-search")
+     */
+
+    public function searchAuthors(Request $request, AuthorRepository $authorRepository, BookRepository $bookRepository){
+        // je récupère les valeurs du formulaire dans ma route
+        $search = $request->query->get('search');
+
+        // je vais créer une méthode dans l'ArticleRepository
+        // qui trouve un article en fonction d'un mot dans son titre ou son contenu
+        $authors = $authorRepository->searchByWord($search);
+        $books = $bookRepository->searchByWord($search);
+
+
+        if((!empty($authors))||(!empty($books))){
+
+            // je renvoie un fichier twig en lui passant les articles trouvé
+            // et je les affiche
+
+            return $this->render('admin/authors-search.html.twig', [
+                'authors' => $authors,
+                'books' => $books
+            ]);
+        }else{
+            //sinon message d'erreur on redirige vers home
+            $this->addFlash('error', 'Votre recherche n\'a rien donné');
+            return $this->redirectToRoute('home');
+        }
+
     }
 
 }

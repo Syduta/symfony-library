4e2e2b084f08c757b3f6133a3faa39bedf30d814
Thibault Touzin <thibault.touzin@gmail.com>
2022-07-21 22:08:46 +0200

    chartreuse mode localhost

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,15 +1,54 @@
-console.log('lol');
-const bouton = document.querySelector('.bttn');
-bouton.addEventListener('click',function (){
-    const links = document.querySelector('.boody');
-    console.log(bouton);
+// console.log('lol');
+//
+// const body = document.querySelector('.body');
+// const chartreuseMode  = localStorage.getItem('color-char')=== 'true';
+//
+//
+// if (chartreuseMode) {
+//     document.querySelector('.body').classList.add('color-char');
+// }
+//
+// const bouton = document.querySelector('.bttn');
+// bouton.addEventListener('click',function (){
+//
+//
+//     if(body.classList.contains("color-char")){
+//         body.classList.remove("color-char");
+//         localStorage.removeItem('color-char');
+//
+//     }else{
+//         body.classList.add("color-char");
+//         localStorage.setItem('color-char', 'true');
+//
+//     }
+// })
+// console.log(localStorage);
+//
+const body = document.querySelector('.js-body');
 
-    if(links.classList.contains("color-char")){
+// je check si le mode chartreuse est activé dans le local storage,
+// si oui je l'active avec le CSS
+const chartreuseMode  = localStorage.getItem('color-char')=== 'true';
+if (chartreuseMode) {
+    body.classList.add('color-char');
+}
 
-        links.classList.remove("color-char");
+const nightToggleBtn = document.querySelector('.js-night-toggle');
 
-    }else{
-        links.classList.add("color-char");
+nightToggleBtn.addEventListener('click', function() {
 
+    // si le mode chartreuse est activé, je le désactive
+    if (body.classList.contains('color-char')) {
+        body.classList.remove('color-char');
+        // je supprime le mode nuit du local storage
+        localStorage.removeItem('color-char');
+        console.log(localStorage);
+        // si le mode chartreuse n'est pas activé, je l'active
+    } else {
+        body.classList.add('color-char');
+        // j'enregistre le mode nuit dans le local storage
+        localStorage.setItem('color-char', "true");
+        console.log(localStorage);
     }
-})
+
+});
